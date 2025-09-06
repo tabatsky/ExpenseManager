01fed514639075adad85b73ca99a9319417014da
Evgeniy Tabatsky <tabatsky.evgeniy@gene.com>
2025-09-07 07:55:31 +1000

    TODO updated

--- src/commonMain/kotlin/Todo.kt
+++ src/commonMain/kotlin/Todo.kt
@@ -1,3 +1,4 @@
 // TODO: get rid of the JVM dependencies and replace them with the pure KMP
 // TODO: adapt app for Android tablets
 // TODO: adapt app for iPad
+// TODO: implement adding and removing categories via UI
