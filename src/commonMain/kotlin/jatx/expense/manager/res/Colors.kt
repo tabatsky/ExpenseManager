9ee2ec84f130a898a50b7c95086856ab4b5e42a8
Evgeniy Tabatsky <tabatsky.evgeniy@gene.com>
2026-02-19 14:08:09 +1000

    a little color changing

--- src/commonMain/kotlin/jatx/expense/manager/res/Colors.kt
+++ src/commonMain/kotlin/jatx/expense/manager/res/Colors.kt
@@ -3,7 +3,7 @@ package jatx.expense.manager.res
 import androidx.compose.ui.graphics.Color
 
 val blueColor = Color(0xFF448BE2)
-val violetColor = Color(0xFF8A30C1)
+val violetColor = Color(0xFFBA60F1)
 val greenColor = Color(0xFF70BB69)
 val redColor = Color(0xFFFF3A00)
 val yellowColor = Color(0xFFDDDD77)
